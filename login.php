986168570ee38dab24c9bd4a4442a5a44fe59b91
Rutujarrajhans <59066365+Rutujarrajhans@users.noreply.github.com>
2021-04-19 23:11:57 +0530

    Update login.php

--- login.php
+++ login.php
@@ -122,7 +122,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <span class="invalid-feedback"><?php echo $password_err; ?></span>
             </div>
             <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Login">
+                <input type="submit" name="submit" class="btn btn-primary" value="Login">
             </div>
             <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
         </form>
